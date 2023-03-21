30642186c1e985238252e058f554d66e36e19fe6
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2023-03-21 16:26:13 +0100

    intersection obs

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,61 +1,16 @@
-const images = document.querySelectorAll(".section__image--js");
-const headers = document.querySelectorAll(".section__header--js");
-const content = document.querySelectorAll(".section__paragraph--js");
-
-const observerImg = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("section--active");
+    } else {
+      entry.target.classList.remove("section--active");
     }
   });
 });
 
-const observerHeaders = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
-    }
-  });
+const sections = document.querySelectorAll(".section");
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
-
-const observerContent = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
-    }
-  });
-});
-
-images.forEach((images) => {
-  observerImg.observe(images);
-});
-headers.forEach((headers) => {
-  observerHeaders.observe(headers);
-});
-content.forEach((content) => {
-  observerContent.observe(content);
-});
-
-console.log("hello");
-
-// function scrollTrigger(selector) {
-//   let els = document.querySelectorAll(selector);
-//   els = Array.from(els);
-//   els.forEach((el) => {
-//     addObserver(el);
-//   });
-// }
-
-// function addObserver(el) {
-//   let observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add("popUp");
-//         observer.unobserve(entry.target);
-//       }
-//     });
-//   });
-//   observer.observe(el);
-// }
-
-// scrollTrigger(".section__image", { rootMargin: "-200px" });
