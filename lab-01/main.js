67c856a453d7d36e0c7e72dc2ca8bf81f2a28ebd
MichalJakobczyk <michal.jakobczyk2@gmail.com>
2023-03-22 14:55:39 +0100

    fix

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,42 +1,42 @@
-const images = document.querySelectorAll(".section__image--js");
-const headers = document.querySelectorAll(".section__header--js");
-const content = document.querySelectorAll(".section__paragraph--js");
+// const images = document.querySelectorAll(".section__image--js");
+// const headers = document.querySelectorAll(".section__header--js");
+// const content = document.querySelectorAll(".section__paragraph--js");
 
-const observerImg = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
-    }
-  });
-});
+// const observerImg = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > 0) {
+//       entry.target.classList.add("popUp");
+//     }
+//   });
+// });
 
-const observerHeaders = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
-    }
-  });
-});
+// const observerHeaders = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > 0) {
+//       entry.target.classList.add("popUp");
+//     }
+//   });
+// });
 
-const observerContent = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add("popUp");
-    }
-  });
-});
+// const observerContent = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > 0) {
+//       entry.target.classList.add("popUp");
+//     }
+//   });
+// });
 
-images.forEach((images) => {
-  observerImg.observe(images);
-});
-headers.forEach((headers) => {
-  observerHeaders.observe(headers);
-});
-content.forEach((content) => {
-  observerContent.observe(content);
-});
+// images.forEach((images) => {
+//   observerImg.observe(images);
+// });
+// headers.forEach((headers) => {
+//   observerHeaders.observe(headers);
+// });
+// content.forEach((content) => {
+//   observerContent.observe(content);
+// });
 
-console.log("hello");
+// console.log("hello");
 
 // function scrollTrigger(selector) {
 //   let els = document.querySelectorAll(selector);
